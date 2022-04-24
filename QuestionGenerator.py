7ca59fb6016aebff358aa88ee90e38d3314b6a99
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2022-04-24 12:49:18 -0500

    remove some comments

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -14,12 +14,6 @@ from nltk.corpus import wordnet as wn
 
 # print(spacy.__version__)
 
-# nlp = spacy.load("en_core_web_sm")
-# doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
-
-# for ent in doc.ents:
-#     print(ent.text, ent.start_char, ent.end_char, ent.label_)
-
 class QuestionGenerator:
     def __init__(self) -> None:
         self.nlp = spacy.load("en_core_web_sm")
