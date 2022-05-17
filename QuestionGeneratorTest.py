37c70267d42f1b97373d3f4565e893a94252b657
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2022-05-16 21:53:12 -0500

    NLP program3

--- QuestionGeneratorTest.py
+++ QuestionGeneratorTest.py
@@ -1,12 +1,19 @@
 from QuestionGenerator import QuestionGenerator
 import spacy
 import nltk
-print(spacy.__version__)
-print(nltk.__version__)
+# print(spacy.__version__)
+# print(nltk.__version__)
 
-qg = QuestionGenerator()
+# qg = QuestionGenerator()
 
-qg.getQuestions('John gave the box to Mary?')
-# qg.getQuestions('The boy runs fast')
-qg.getQuestions('Apple released new product last year')
+# qg.getQuestions('John gave the box to Mary?')
+# # qg.getQuestions('The boy runs fast')
+# qg.getQuestions('Apple released new product last year')
+
+
+
+import itertools as it
+
+for num in it.count(10):
+    print(num)
 
